3b5d94ce030b00f2f09c994ff9e7359e9c38dabd
Bobby Ciullo <bobby.ciullo@ucf.edu>
2021-05-14 09:31:34 -0400

    initial commit

---
+++ 01FALSC_UCF-UCF/js/custom.js
@@ -0,0 +1,39 @@
+(function(){
+"use strict";
+'use strict';
+
+var app = angular.module('viewCustom', ['angularLoad']);
+
+"use strict";
+'use strict';
+
+//Auto generated code by primo app store DO NOT DELETE!!! -START-
+/*
+    hookName is a place holder with should hold the hook name not including "prm" at the beginning and in upper camel case
+    e.g: for hook prmSearchBarAfter (in html prm-search-bar-after) it should be given "SearchBarAfter"
+ */
+app.controller('SearchBookmarkFilterAfterController', [function () {
+    var vm = this;
+}]);
+
+app.component('prmSearchBookmarkFilterAfter', {
+    bindings: { parentCtrl: '<' },
+    controller: 'SearchBookmarkFilterAfterController',
+    template: '\n    <primo-explore-help-menu parent-ctrl="$ctrl.parentCtrl"></primo-explore-help-menu>\n'
+
+});
+
+//Auto generated code by primo app store DO NOT DELETE!!! -END-
+
+//Auto generated code by primo app store DO NOT DELETE!!! -START-
+app.constant('primoExploreHelpMenuStudioConfig', [{ "logToConsole": false, "publishEvents": false, "helpMenuTitle": "Search Help", "helpMenuWidth": 500 }]);
+//Auto generated code by primo app store DO NOT DELETE!!! -END-
+})();
+
+
+// libraryh3lp chat box code
+(function() {
+    var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
+    x.src = (document.location.protocol === "https:" ? "https://" : "https://") + "us.libraryh3lp.com/js/libraryh3lp.js?34335"
+    var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
+  })();
